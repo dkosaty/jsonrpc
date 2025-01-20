0bf4c40c7c1a1aaf483e0c25bed21043115644e5
dkosaty <dkosaty@yandex.ru>
2025-01-20 23:09:39 +0300

    Add jsoncpp as cmake library

--- README.md
+++ README.md
@@ -5,5 +5,5 @@ The implementation of JSON-RPC 2.0 Specification.
 https://www.jsonrpc.org/specification
 
 ## Сборка
-1. Install package libjsoncpp-dev
+1. Install package libjsoncpp-dev, libgtest-dev
 2. mkdir build && cd build && cmake -DBUILD_TESTING=ON .. && cmake --build .
--- cmake/FindJsonCpp.cmake
+++ cmake/FindJsonCpp.cmake
@@ -1,8 +1,27 @@
-include (FindPackageHandleStandardArgs)
+include(FindPackageHandleStandardArgs)
+include(GNUInstallDirs)
 
 if (NOT JSONCPP_FOUND)
-  find_path(JSONCPP_INCLUDE_DIRS NAMES jsoncpp/json/json.h PATHS /usr PATH_SUFFIXES include)
-  find_library(JSONCPP_LIBRARIES NAMES jsoncpp PATHS /usr PATH_SUFFIXES lib)
+  find_path(JSONCPP_INCLUDE_DIRS
+    NAMES jsoncpp/json/json.h
+    PATHS /usr
+    PATH_SUFFIXES ${CMAKE_INSTALL_INCLUDEDIR}
+  )
+
+  find_library(JSONCPP_LIBRARIES
+    NAMES jsoncpp
+    PATHS /usr
+    PATH_SUFFIXES ${CMAKE_INSTALL_LIBDIR}
+  )
+
   find_package_handle_standard_args(JsonCpp DEFAULT_MSG JSONCPP_LIBRARIES JSONCPP_INCLUDE_DIRS)
+
   mark_as_advanced(JSONCPP_INCLUDE_DIRS JSONCPP_LIBRARIES)
+
+  add_library(JsonCpp::JsonCpp SHARED IMPORTED)
+
+  set_target_properties(JsonCpp::JsonCpp PROPERTIES
+    INTERFACE_INCLUDE_DIRECTORIES ${JSONCPP_INCLUDE_DIRS}
+    IMPORTED_LOCATION ${JSONCPP_LIBRARIES}
+  )
 endif ()
